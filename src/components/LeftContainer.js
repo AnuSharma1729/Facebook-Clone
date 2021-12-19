4c2fb57f92157b3520c10dcb4d80b9df3a9e660d
AnuSharma1729 <sharmaanu2002in@gmail.com>
2021-12-19 20:59:57 +0530

    make changes

--- src/components/LeftContainer.js
+++ src/components/LeftContainer.js
@@ -7,7 +7,7 @@ const data = [
     {
         'id':1,
         'img':'https://media-exp1.licdn.com/dms/image/C5603AQF-IuuNt18UrA/profile-displayphoto-shrink_400_400/0/1621611802448?e=1633564800&v=beta&t=mniGlAVfwO-fDkPtp56OHkRCqnanCQOKbh4YNoVxru4',
-        'title': 'Gaurav Konde'
+        'title': 'Anu Sharma'
     },
     {
         'id':2,
